61c05aeb871167793e8b69787ebf1696529d1544
Asmaa Algharib <asmaa.samir.algharib@gmail.com>
2025-07-21 06:38:58 -0900

    angular routing

--- client/src/app/core/services/shop-service.ts
+++ client/src/app/core/services/shop-service.ts
@@ -42,6 +42,10 @@ export class ShopService {
     return this.http.get<Pagination<Product>>(this.baseUrl + 'products', { params })
   }
 
+  getProduct(id: number) {
+    return this.http.get<Product>(this.baseUrl + 'products/' + id);
+  }
+
   getBrands() {
     debugger;
     if (this.brands.length > 0) return;
